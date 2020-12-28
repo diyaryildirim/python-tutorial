325334cfc9d4bfb79db9c05f4cd1b066f3c7bbca
diyaryildirim <diyary2000@gmail.com>
2020-12-28 15:02:01 +0300

    new version

---
+++ textcounterv2.py
@@ -0,0 +1,25 @@
+count_words = 0
+count_ve = 0
+count_de = 0
+count_ki = 0
+with open("myfile.txt", 'r') as f:
+    for line in f:
+        words = line.split()
+        count_words += len(words)
+        for i in range(0,len(words)):
+            if(words[i]=='ki'):
+                count_ki+=1;
+            if(words[i]=='de'):
+                count_de+=1
+            if(words[i]=='ve'):
+                count_ve+=1
+
+print("kelime sayisi :")
+print(count_words)
+print("de sayisi :")
+print(count_de)
+print("ki sayisi :")
+print(count_ki)
+print("ve sayisi :")
+print(count_ve)
+input()
